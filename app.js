a4f5fb0151a42381f6649904c3d9d54c82a62faf
yourbeloved02 <rb6233075@gmail.com>
2025-03-16 22:59:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ button.addEventListener("click", function(){
     button.style.transform = "scale(0.8)";
     setTimeout(()=>{
         button.style.transform = "scale(1)";
-        window.location.href = "./love/love.html"
+        window.location.href = "love.html"
     },200)
 })
 
